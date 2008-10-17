81836fe65aa163c711e28590ee4f24efc45b2bcc
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-17 16:57:54 -0700

    Added a basic :user_vendor_invoices permission and added a top menu for the plugin. #1650

--- init.rb
+++ init.rb
@@ -8,4 +8,8 @@ Redmine::Plugin.register :redmine_billing do
   author 'Author name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
+
+  permission(:use_vendor_invoices, { :vendor_invoices => [:index, :show, :new, :create, :edit, :update, :destroy] })
+
+  menu :top_menu, :vendor_invoices, {:controller => 'vendor_invoices', :action => 'index'}, :caption => :list_vendor_invoices
 end
