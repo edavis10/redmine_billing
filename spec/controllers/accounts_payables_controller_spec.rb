10af326c2e24e56a78e3c7ae69713f6d209a0433
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-23 09:22:04 -0700

    Added pending specs for the Accounts Payable Controller. #1650

--- spec/controllers/accounts_payables_controller_spec.rb
+++ spec/controllers/accounts_payables_controller_spec.rb
@@ -8,3 +8,72 @@ describe AccountsPayablesController do
   end
 
 end
+
+describe AccountsPayablesController, "#index" do
+  it 'should be successful'
+
+  it 'should load all vendor invoices'
+
+  it 'should render the index template'
+end
+
+describe AccountsPayablesController, "#show" do
+  it 'should be successful'
+
+  it 'should load the vendor invoice'
+
+  it 'should render the show template'
+end
+
+describe AccountsPayablesController, "#new" do
+  it 'should be successful'
+
+  it 'should load a new vendor invoice'
+
+  it 'should render the new template'
+end
+
+describe AccountsPayablesController, "#create with successful save" do
+  it 'should redirect to the vendor invoice'
+
+  it 'should save the vendor invoice'
+
+  it 'should set the flash message'
+end
+
+describe AccountsPayablesController, "#create with unsuccessful save" do
+  it 'should redirect to the new vendor invoice form'
+
+  it 'should not save the vendor invoice'
+end
+
+describe AccountsPayablesController, "#edit" do
+  it 'should be successful'
+
+  it 'should load the vendor invoice'
+
+  it 'should render the edit template'
+end
+
+describe AccountsPayablesController, "#update with successful save" do
+  it 'should redirect to the vendor invoice'
+
+  it 'should update the vendor invoice'
+
+  it 'should set the flash message'
+end
+
+describe AccountsPayablesController, "#update with unsuccessful save" do
+  it 'should redirect to the edit vendor invoice form'
+
+  it 'should not update the vendor invoice'
+end
+
+describe AccountsPayablesController, "#destroy" do
+  it 'should redirect to the vendor invoices list'
+
+  it 'should delete the vendor invoice'
+
+  it 'should set the flash message'
+end
+
