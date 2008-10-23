c7c49081d61e00c1f67076be7f84fc3f88e2ce0c
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-23 11:52:48 -0700

    Added basic validations for a VendorInvoice. #1650

--- app/models/vendor_invoice.rb
+++ app/models/vendor_invoice.rb
@@ -1,3 +1,7 @@
 class VendorInvoice < ActiveRecord::Base
   has_and_belongs_to_many :users
+  
+  validates_presence_of :number
+  validates_presence_of :invoiced_on
+  validates_presence_of :billing_status
 end
