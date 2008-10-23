bc55588df77793ff37f16d4129965435e6554123
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-23 12:18:40 -0700

    billing_type is now set when a VendorInvoice is saved, based on it's amount. #1650

--- app/models/vendor_invoice.rb
+++ app/models/vendor_invoice.rb
@@ -4,4 +4,16 @@ class VendorInvoice < ActiveRecord::Base
   validates_presence_of :number
   validates_presence_of :invoiced_on
   validates_presence_of :billing_status
+  
+  before_save :set_billing_type
+  
+  def set_billing_type
+    if !self.read_attribute(:amount).nil? && self.read_attribute(:amount) > 0
+      self.write_attribute(:billing_type, 'fixed')
+    else
+      self.write_attribute(:billing_type, 'hourly')
+    end
+
+    return true
+  end
 end
--- spec/models/vendor_invoice_spec.rb
+++ spec/models/vendor_invoice_spec.rb
@@ -1,4 +1,42 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 module VendorInvoiceSpecHelper
+  # TODO: Semi-Duplicated
+  def vendor_invoice_object_factory(id, options = { })
+    object_options = { 
+      :id => id,
+      :number => '9000',
+      :invoiced_on => Date.today
+    }.merge(options)
+    
+    vendor_invoice = VendorInvoice.new object_options
+    return vendor_invoice
+  end
+end
+
+describe VendorInvoice, 'when saving' do
+  include VendorInvoiceSpecHelper
+  
+  it 'should set the type to fixed if amount is present' do
+    vendor_invoice = vendor_invoice_object_factory(1,
+                                                   {
+                                                     :amount => 200.00,
+                                                     :billing_status => 'unbilled'})
+
+    vendor_invoice.save.should eql(true)
+    vendor_invoice.reload
+    vendor_invoice.billing_type.should eql('fixed')
+  end
+
+  it 'should set the type to hourly if amount is absent' do
+    vendor_invoice = vendor_invoice_object_factory(1,
+                                                   {
+                                                     :amount => nil,
+                                                     :billing_status => 'unbilled'})
+
+    vendor_invoice.save.should eql(true)
+    vendor_invoice.reload
+    vendor_invoice.billing_type.should eql('hourly')
+
+  end
 end
