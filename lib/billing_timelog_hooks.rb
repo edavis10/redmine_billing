d69097d857126d67ae4963fc4135c74bc45c7478
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-28 15:04:50 -0700

    Refactored content_tag out so we can easily add more html. #1670

--- lib/billing_timelog_hooks.rb
+++ lib/billing_timelog_hooks.rb
@@ -5,18 +5,20 @@ class BillingTimelogHooks < Redmine::Hook::ViewListener
     form = context[:form]
 
     o = ''
-    o << content_tag(:p,
-                       text_field_with_auto_complete(:time_entry,
-                                                     :vendor_invoice_id,
-                                                     { :autocomplete => "off", :size => 35 },
-                                                     { :url => {
-                                                         :controller => 'accounts_payables',
-                                                         :action => 'auto_complete_for_vendor_invoice_number',
-                                                         :protocol => Setting.protocol,
-                                                         :host => Setting.host_name
-                                                       }}))
+    # Create a text field for vendor_invoice_id
+    o << "<p>"
+    o << text_field_with_auto_complete(:time_entry,
+                                       :vendor_invoice_id,
+                                       { :autocomplete => "off", :size => 35 },
+                                       { :url => {
+                                           :controller => 'accounts_payables',
+                                           :action => 'auto_complete_for_vendor_invoice_number',
+                                           :protocol => Setting.protocol,
+                                           :host => Setting.host_name
+                                         }})
     
     
+    o << "</p>"
     return o
   end
 end
