264bf4426c83f5714310731850b527606ec93630
Eric Davis <edavis@littlestreamsoftware.com>
2008-10-28 13:50:01 -0700

    Added auto_complete plugin. #1670

--- init.rb
+++ init.rb
@@ -8,6 +8,17 @@ require_dependency 'billing_timelog_hooks'
 require_dependency 'billing_user_patch'
 require_dependency 'billing_time_entry_patch'
 
+# Plugins
+Dir[File.join(directory,'vendor','plugins','*')].each do |dir|
+  path = File.join(dir, 'lib')
+  $LOAD_PATH << path
+  Dependencies.load_paths << path
+  Dependencies.load_once_paths.delete(path)
+end
+
+# AutoComplete
+require File.join(directory,'vendor','plugins','auto_complete','init')
+
 Redmine::Plugin.register :redmine_billing do
   name 'Redmine Billing plugin'
   author 'Author name'
