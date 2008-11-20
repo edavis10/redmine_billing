b74e54634032d09e4dbe7e58f7dd9c4e2a65f59a
Eric Davis <edavis@littlestreamsoftware.com>
2008-11-19 17:20:07 -0800

    Implemented a few specs for the response to collect_member_data

--- spec/models/selected_time_entry_spec.rb
+++ spec/models/selected_time_entry_spec.rb
@@ -11,6 +11,58 @@ describe SelectedTimeEntry, 'collect_member_data' do
     @selected_time_entry.collect_member_data.should be_a_kind_of(Array)
   end
   
+  it 'should contain a record per User' do
+    @user_one = mock_model(User)
+    @user_one.stub!(:name).and_return("Test User")
+    @time_entry_one = mock_model(TimeEntry, :id => 1)
+    @time_entry_one.stub!(:user).and_return(@user_one)
+    
+    @user_two = mock_model(User)
+    @user_two.stub!(:name).and_return("Test User Two")
+    @time_entry_two = mock_model(TimeEntry, :id => 2)
+    @time_entry_two.stub!(:user).and_return(@user_two)
+
+    @selected_time_entry = SelectedTimeEntry.new
+    @selected_time_entry.time_entries = [@time_entry_one, @time_entry_two]
+    @selected_time_entry.collect_member_data.should have(2).things
+    
+  end
+  
+  describe 'response record' do
+    it 'should have the user name' do
+      @user = mock_model(User)
+      @user.should_receive(:name).and_return("Test User")
+      @time_entry_one = mock_model(TimeEntry, :id => 1)
+      @time_entry_one.should_receive(:user).and_return(@user)
+      
+      @selected_time_entry = SelectedTimeEntry.new
+      @selected_time_entry.time_entries = [@time_entry_one]
+      response = @selected_time_entry.collect_member_data
+      response.should have_at_least(1).thing
+      response[0][:name].should eql('Test User')
+    end
+
+    it 'should have the count of time_entries for the user'
+
+    it 'should have the total time for the user' do
+      @user = mock_model(User)
+      @user.stub!(:name).and_return("Test User")
+      @time_entry_one = mock_model(TimeEntry, :id => 1)
+      @time_entry_one.should_receive(:user).and_return(@user)
+      
+      @selected_time_entry = SelectedTimeEntry.new
+      @selected_time_entry.time_entries = [@time_entry_one]
+      @selected_time_entry.should_receive(:total_of_user_time_entries).and_return(4.0)
+      response = @selected_time_entry.collect_member_data
+      response.should have_at_least(1).thing
+      response[0][:time].should eql(4.0)
+    end
+
+    it 'should have an amount for the user'
+    it 'should format the amount into currency'
+    it 'should format time into a hundredths'
+  end
+  
   it 'should select all the Users from the Time Entries' do
     @user = mock_model(User)
     @user.stub!(:name).and_return("Test User")
