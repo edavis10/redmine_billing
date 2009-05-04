da7cd9902f4f08068ae74fc61230ce2099641936
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-04 11:14:01 -0700

    Separate init.rb's actions to be more readable.

--- init.rb
+++ init.rb
@@ -29,7 +29,25 @@ Redmine::Plugin.register :redmine_billing do
   description 'This is a plugin for Redmine'
   version '0.3.0'
 
-  permission(:use_accounts_payable, { :accounts_payables => [:index, :new, :create, :edit, :update, :destroy, :context_menu, :bulk_edit, :bulk_update, :auto_complete_for_vendor_invoice_number, :timesheet, :update_time_entries, :unbilled_po, :unspent_labor, :unbilled_labor, :time_counter] })
+  permission(:use_accounts_payable, {
+               :accounts_payables => [
+                                      :auto_complete_for_vendor_invoice_number,
+                                      :bulk_edit,
+                                      :bulk_update,
+                                      :context_menu,
+                                      :create,
+                                      :destroy,
+                                      :edit,
+                                      :index,
+                                      :new,
+                                      :time_counter,
+                                      :timesheet,
+                                      :unbilled_labor,
+                                      :unbilled_po,
+                                      :unspent_labor,
+                                      :update,
+                                      :update_time_entries
+                                     ] })
 
   # Allows a user to see all invoices for the project they have this Role+Permission on
   permission :all_invoices_on_project, { }
