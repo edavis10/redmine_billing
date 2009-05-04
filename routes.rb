aaba415fca19078fb10c3d4b365be52b5e677596
Eric Davis <edavis@littlestreamsoftware.com>
2009-05-04 11:05:46 -0700

    Reformatted the routes to be more readable

--- routes.rb
+++ routes.rb
@@ -1,1 +1,13 @@
-resources :accounts_payables, :collection => { :context_menu => :post, :bulk_edit => :get, :bulk_update => :put, :auto_complete_for_vendor_invoice_number => :get, :timesheet => :get, :update_time_entries => :put, :unbilled_po => :get, :unspent_labor => :get, :unbilled_labor => :get, :time_counter => :get }
+resources(:accounts_payables,
+          :collection => {
+            :context_menu => :post,
+            :bulk_edit => :get,
+            :bulk_update => :put,
+            :auto_complete_for_vendor_invoice_number => :get,
+            :timesheet => :get,
+            :update_time_entries => :put,
+            :unbilled_po => :get,
+            :unspent_labor => :get,
+            :unbilled_labor => :get,
+            :time_counter => :get
+          })
