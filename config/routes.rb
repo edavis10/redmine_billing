e257439772aa90b92a918237b3344a56f816957b
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-21 11:39:21 -0700

    Added Rails 2.3.4 routes

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resources(:accounts_payables,
+                :collection => {
+                  :context_menu => :post,
+                  :bulk_edit => :get,
+                  :bulk_update => :put,
+                  :auto_complete_for_vendor_invoice_number => :get,
+                  :timesheet => :get,
+                  :update_time_entries => :put,
+                  :unbilled_po => :get,
+                  :unspent_labor => :get,
+                  :unbilled_labor => :get,
+                  :time_counter => :get,
+                  :filter => :post
+                })
+end
